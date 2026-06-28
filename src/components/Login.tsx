d9645f4e1851a989e316bfa52de748bc0cdf5f0f
Siddharth Dronachary <siddudrona@gmail.com>
2026-06-28 14:52:03 +0530

    feat: add user authentication components and global styles

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,82 +2,54 @@
 import appwriteService from "@/appwrite/config";
 import useAuth from "@/context/useAuth";
 import Link from "next/link";
-import Image from "next/image";
 import { useRouter } from "next/navigation";
 import React, { FormEvent, useState } from "react";
 
-const Signup = () => {
+const Login = () => {
   const router = useRouter();
+  const { setAuthStatus } = useAuth();
   const [formData, setFormData] = useState({
     email: "",
     password: "",
-    name: "",
   });
   const [error, setError] = useState("");
 
-  const { setAuthStatus } = useAuth();
-
-  const create = async (e: FormEvent<HTMLFormElement>) => {
+  const login = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
-      const userData = await appwriteService.createUserAccount(formData);
-      if (userData) {
+      const session = await appwriteService.login(formData);
+      if (session) {
         setAuthStatus(true);
         router.push("/profile");
       }
-    } catch (error: unknown) {
-      if (error instanceof Error) {
-        setError(error.message);
-      } else {
-        setError(String(error));
-      }
+    } catch (error: any) {
+      setError(error.message);
     }
   };
 
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex items-center justify-center w-full">
       <div className={`mx-auto w-full max-w-lg bg-gray-200/50 rounded-xl p-10`}>
         <div className="mb-2 flex justify-center">
           <span className="inline-block w-full max-w-15">
-            <Image src="/favicon.ico" alt="Logo" width={48} height={48} />
+            <img src="/favicon.ico" alt="Logo" />
           </span>
         </div>
         <h2 className="text-center text-2xl font-bold leading-tight text-black">
-          Sign up to create account
+          Sign in to your account
         </h2>
         <p className="mt-2 text-center text-base text-gray-600">
-          Already have an account?&nbsp;
+          Don&apos;t have any account?&nbsp;
           <Link
-            href="/login"
+            href="/signup"
             className="font-medium text-primary transition-all duration-200 hover:underline"
           >
-            Sign In
+            Sign Up
           </Link>
         </p>
         {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
-        <form onSubmit={create} className="mt-8">
+        <form onSubmit={login} className="mt-8">
           <div className="space-y-5">
-            <div>
-              <label
-                htmlFor="name"
-                className="text-base font-medium text-gray-900"
-              >
-                Full Name
-              </label>
-              <div className="mt-2">
-                <input
-                  className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-                  type="text"
-                  placeholder="Full Name"
-                  id="name"
-                  value={formData.name}
-                  onChange={(e) =>
-                    setFormData((prev) => ({ ...prev, name: e.target.value }))
-                  }
-                  required
-                />
-              </div>
-            </div>
             <div>
               <label
                 htmlFor="email"
@@ -130,7 +102,7 @@ const Signup = () => {
                 type="submit"
                 className="inline-flex w-full items-center justify-center rounded-md bg-primary px-3.5 py-2.5 font-semibold leading-7 text-white hover:bg-primary/80"
               >
-                Create Account
+                Sign in
               </button>
             </div>
           </div>
@@ -139,5 +111,3 @@ const Signup = () => {
     </div>
   );
 };
-
-export default Signup;
